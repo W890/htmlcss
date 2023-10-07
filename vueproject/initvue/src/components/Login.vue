cb469d0b54b21353ab2b100525041fb092f94aa4
wykker <13381340+wykker@user.noreply.gitee.com>
2023-10-07 19:03:26 +0800

    ome

---
+++ vueproject/initvue/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template><!--vue实例外创建 定义组件-->
+    <div>
+        <h1 class="titlel">
+            我是登录组件的
+        </h1>
+    </div>
+</template>
+<script>
+    export default{
+        name:'Login',
+        props:['text'],
+        methods:{
+           
+        }
+    }
+</script>
+<style>
+.titlel{
+    color: red;
+}
+</style>
---
+++ vueproject/initvue/src/main.js
@@ -0,0 +1,19 @@
+// 引入vue
+import Vue from 'vue'
+// 引入app组件，它是所有组件的父组件
+import App from './App.vue'
+// 关闭生产提示
+Vue.config.productionTip = false
+
+import axios from 'axios'
+// 配置请求的跟路径
+// axios.defaults.baseURL='http://192.168.0.88:2000/weather'
+axios.defaults.baseURL='http://apis.juhe.cn/simpleWeather'
+
+// 将请求挂载到vue原型上
+Vue.prototype.$http = axios
+// 创建vue
+new Vue({
+  render: h => h(App)
+}).$mount('#app')
+ 
